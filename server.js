ca193ec99e50b4b38daf2df10b4ed2db11face55
Belle Nguyen <NgBelleK@gmail.com>
2020-08-07 15:17:19 -0500

    create a working front-end using react and webpack

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const parth = require('path');
+const path = require('path');
 
 const PORT = 5000;
 const app = express();
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.listen(PORT);
 
-app.get('/', (req, res) => {
-  res.send({working: true})
+app.get('/home', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/public/index.html'))
 })
