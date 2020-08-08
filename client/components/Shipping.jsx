debfdebb665677805e159d4b447c6e41900f9e68
Belle Nguyen <NgBelleK@gmail.com>
2020-08-08 10:46:05 -0500

    Fix spacing

--- client/components/Shipping.jsx
+++ client/components/Shipping.jsx
@@ -2,10 +2,9 @@ const React = require('react');
 
 const Shipping = () => {
 
-return (
-  <div>This is the Shipping Component</div>
-)
-
+  return (
+    <div>This is the Shipping Component</div>
+  )
 }
 
 export default Shipping;
