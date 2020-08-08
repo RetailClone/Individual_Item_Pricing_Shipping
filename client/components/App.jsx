57e11262ad4ea317079474071698b3721f8433d8
Belle Nguyen <NgBelleK@gmail.com>
2020-08-08 10:14:39 -0500

    Add constructor to App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,11 @@
 const React = require('react');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <h1>Working on Component</h1>
