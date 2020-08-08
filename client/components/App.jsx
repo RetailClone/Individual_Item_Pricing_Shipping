ee5894fdb98d0b8a98ee3c7183a4f76e5915f3f5
Belle Nguyen <NgBelleK@gmail.com>
2020-08-08 09:56:02 -0500

    Render a simple message

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ const React = require('react');
 class App extends React.Component {
   render() {
     return (
-      <h1>HIHI</h1>
+      <h1>Hello World</h1>
     )
   }
 }
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const path = require('path');
-
 const PORT = 7770;
+
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.listen(PORT);
 
-app.get('/home', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/public/index.html'))
-})
+app.listen(PORT, ()=> console.log(`Individual item server listening on port: ${PORT}`));
