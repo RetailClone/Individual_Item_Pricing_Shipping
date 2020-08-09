686ec4486ea4b39f45416653a778a2ceda279018
Belle Nguyen <NgBelleK@gmail.com>
2020-08-09 16:34:40 -0500

    Create getPrice query function for server

--- database/queries.js
+++ database/queries.js
@@ -3,4 +3,16 @@ const databaseConfig = require('./db.config.js');
 
 const connection = mysql.createConnection(databaseConfig);
 
+const getPrice = (id, callback) => {
+  connection.query(`SELECT price FROM products WHERE id = ${id}`, (err, results, fields) => {
+    if (err) {
+      console.log("Query error in getting price", err);
+      callback(err, null);
+    } else {
+      console.log("Success query for price");
+      callback(null, results);
+    }
+  });
+};
+
 module.exports = {};
