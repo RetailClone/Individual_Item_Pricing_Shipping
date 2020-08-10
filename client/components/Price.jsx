a6b8fc602ec155c8187fb113bfeebbc561022a7c
Belle Nguyen <NgBelleK@gmail.com>
2020-08-09 20:19:19 -0500

    Create frontend Price get request

--- client/components/Price.jsx
+++ client/components/Price.jsx
@@ -5,31 +5,32 @@ class Price extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId : 1
+      productId : 1,
+      price: 0
     };
     this.getPrice = this.getPrice.bind(this);
   }
 
+  componentDidMount() {
+    this.getPrice(this.state.productId);
+  }
+
   getPrice(prodId) {
-    // Axios.get('https://localhost:7770/product/price', {
-    //   params: {
-    //     Id: prodId
-    //   }
-    // })
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-    return prodId;
+    Axios.get(`http://localhost:7770/product/price/${prodId}`)
+      .then ( (response) => {
+      this.setState({price: response.data.price});
+      console.log("price of item", this.state.price);
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
   }
 
   render() {
     return (
       <div>
         <div>This is the Price Component</div>
-        {this.getPrice(this.state.productId)}
+        {/* {this.getPrice(this.state.productId)} */}
       </div>
     )
   }
