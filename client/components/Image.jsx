2b4782e3b26e39a35a6c29d10f8b10d6d9d023f6
Belle Nguyen <NgBelleK@gmail.com>
2020-08-09 21:17:51 -0500

    Create getPhotos request function

--- client/components/Image.jsx
+++ client/components/Image.jsx
@@ -4,12 +4,39 @@ import Axios from 'axios';
 class Image extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      productId: 1,
+      photosList: [{link: "https://via.placeholder.com/300"}]
+    };
+    this.getPhotos = this.getPhotos.bind(this);
   }
+
+  componentDidMount() {
+    this.getPhotos(this.state.productId);
+  }
+
+  getPhotos(prodId) {
+    Axios.get(`http://localhost:7770/product/photos/${prodId}`)
+      .then ( (response) => {
+      this.setState({photosList: response.data});
+      console.log("Array of photo links", this.state.photosList);
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
+  }
+
+
 
   render() {
     return (
-      <div>This is the Image Component</div>
+      <div>
+        <div>This is the Image Component</div>
+        <div>Name of Item goes here</div>
+        <div>List of images</div>
+        <div>Main Image goes here</div>
+
+      </div>
     )
   }
 }
