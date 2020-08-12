4b44519ad1224c5261a43cd1c746917530966f4b
Belle Nguyen <NgBelleK@gmail.com>
2020-08-11 20:22:28 -0500

    fix code syntax

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
-import React from ("react");
-import Image from "./Image.jsx";
-import Price from "./Price.jsx";
-import Shipping from "./Shipping.jsx";
-import axios from "axios";
+import React from 'react';
+import Image from './Image.jsx';
+import Price from './Price.jsx';
+import Shipping from './Shipping.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       productId: 40,
-      name: "Product Name",
+      name: 'Product Name',
     };
     this.getName = this.getName.bind(this);
   }
@@ -21,25 +21,27 @@ class App extends React.Component {
   //sends request to retrieve name of product
   getName(prodId) {
     axios.get(`http://localhost:7770/product/name/${prodId}`)
-      .then((response) => {
-        this.setState({ name: response.data.name });
+      .then ( (response) => {
+      this.setState({name: response.data.name});
       })
-      .catch((error) => {
+      .catch( (error) => {
         console.log(error);
-      });
+      })
   }
 
   render() {
     return (
       <div className="individual-items-main-view">
-        <div className="product-name">{this.state.name}</div>
+        <div className="product-name">
+          {this.state.name}
+        </div>
         <div className="individual-items-content">
-          <Image itemId={this.state.productId} />
-          <Price itemId={this.state.productId} />
-          <Shipping />
+          <Image itemId={this.state.productId}/>
+          <Price itemId={this.state.productId}/>
+          <Shipping/>
         </div>
       </div>
-    );
+    )
   }
 }
 
