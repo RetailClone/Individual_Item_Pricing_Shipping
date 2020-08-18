015d366e1dec9705276d5eb0c7f7e25e0c01a4d3
Belle Nguyen <NgBelleK@gmail.com>
2020-08-18 09:59:42 -0500

    change localhost to ec2

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import Image from './Image.jsx';
-import Price from './Price.jsx';
-import Shipping from './Shipping.jsx';
-import axios from 'axios';
+import React from "react";
+import Image from "./Image.jsx";
+import Price from "./Price.jsx";
+import Shipping from "./Shipping.jsx";
+import axios from "axios";
 import styles from "../style.css";
 
 class ItemView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Product Name',
+      name: "Product Name",
     };
     this.getName = this.getName.bind(this);
     this.getProductId = this.getProductId.bind(this);
   }
 
   componentDidMount() {
+    // sets default baseURL for ALL axios reqs
+    // sets default baseURL for ALL axios reqs
+    axios.defaults.baseURL =
+      "http://ec2-18-191-90-13.us-east-2.compute.amazonaws.com:7770";	      "http://ec2-18-191-7-97.us-east-2.compute.amazonaws.com:1701";
+      //"http://localhost:7770";
     this.getName();
   }
 
@@ -27,28 +32,27 @@ class ItemView extends React.Component {
 
   //sends request to retrieve name of product
   getName() {
-    axios.get(`http://localhost:7770/product/name/${this.getProductId()}`)
-      .then ( (response) => {
-      this.setState({name: response.data.name});
+    axios
+      .get(`/product/name/${this.getProductId()}`)
+      .then((response) => {
+        this.setState({ name: response.data.name });
       })
-      .catch( (error) => {
+      .catch((error) => {
         console.log(error);
-      })
+      });
   }
 
   render() {
     return (
       <div className={styles.individualItemsMainView}>
-        <div className={styles.productName}>
-          {this.state.name}
-        </div>
+        <div className={styles.productName}>{this.state.name}</div>
         <div className={styles.individualItemsContent}>
-          <Image productId={this.getProductId()}/>
-          <Price productId={this.getProductId()}/>
-          <Shipping/>
+          <Image productId={this.getProductId()} />
+          <Price productId={this.getProductId()} />
+          <Shipping />
         </div>
       </div>
-    )
+    );
   }
 }
 
